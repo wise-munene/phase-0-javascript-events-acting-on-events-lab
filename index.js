b88108a7fb7542d173137e4174b4436a200f19b7
Wise Munene Mutuku <wisemunene99@gmail.com>
2025-09-14 16:38:16 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+function moveDodgerRight() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  // game width = 400px, dodger width = 40px → max left = 360
+  if (left < 360) {
+    dodger.style.left = `${left + 1}px`;
+  }
+}
+
+function addingEventListener() {
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    } else if (e.key === "ArrowRight") {
+      moveDodgerRight();
+    }
+  });
+}
